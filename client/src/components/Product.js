8bc9cd0988e7d20630bb5a092bda76844b2e8c50
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2023-06-27 16:39:07 +0530

    TopCom

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -6,7 +6,7 @@ const Product = ({ product }) => {
   // console.log(product);
   return (
     <Link to={`/product/${product.id}`}>
-      <div className='grad w-[206px] h-[362px] rounded-[8px] overflow-hidden relative group'>
+      <div className='grad w-full h-[362px] rounded-[8px] overflow-hidden relative group'>
         {/* badge */}
         {product.attributes.isNew ? (
           <div className='absolute bg-accent text-primary text-[12px] font-extrabold uppercase top-4 right-4 px-2 rounded-full z-10'>
@@ -16,7 +16,7 @@ const Product = ({ product }) => {
           ''
         )}
         {/* image */}
-        <div className='w-[200px] h-[200px] flex items-center justify-center relative'>
+        <div className='w-full h-[200px] flex items-center justify-center relative'>
           <img
             className='w-[160px] h-[160px] group-hover:scale-90 transition-all'
             src={`${product.attributes.image.data.attributes.url}`}
