435939900b3f2d8d177c7719c091955a10d13c8f
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2023-06-29 22:39:26 +0530

    SEO Check

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { HelmetProvider } from 'react-helmet-async'; // Import HelmetProvider
 // cart provider
 import CartProvider from './context/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <CartProvider>
-      <App />
-    </CartProvider>
+    <HelmetProvider> {/* Wrap your components with HelmetProvider */}
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </HelmetProvider>
   </React.StrictMode>
 );
