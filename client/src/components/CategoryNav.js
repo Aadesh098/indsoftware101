b3fd67b2eb6d46e53319def4acdde5838d03a29d
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2023-06-29 22:45:59 +0530

    Com101

--- client/src/components/CategoryNav.js
+++ client/src/components/CategoryNav.js
@@ -6,8 +6,7 @@ import { Link } from "react-router-dom";
 
 const CategoryNav = () => {
   const { data } = useFetch("/categories");
-  console.log(data);
-  return (
+    return (
     <aside className="hidden xl:flex">
       <div className="bg-primary flex flex-col w-[286px] h-[500px] rounded-[8px] overflow-hidden">
         <div className="bg-accent py-4 text-primary uppercase font-semibold flex items-center justify-center">
