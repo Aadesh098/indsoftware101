f8fdf536c18b29d0badf3bd8ba9a7d63471b264a
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2023-07-03 15:31:49 +0530

    Button Check

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -54,7 +54,7 @@ const ProductDetails = () => {
 
               <button
                 // onClick={() => addToCart(data, id)}
-                className="btn2 btn-accent"
+                className="btn2 md:btn btn-accent"
               >
                 <a href={data[0].attributes.paymentlink}>{buttonText}</a>
               </button>
