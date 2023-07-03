e25c081b08f4701817813fcc3538a00aa736498c
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2023-07-03 14:28:15 +0530

    Sitemap

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import { renderToStaticMarkup } from 'react-dom/server';
 
 // pages
 import Home from './pages/Home';
@@ -8,15 +8,13 @@ import Products from './pages/Products';
 import ProductDetails from './pages/ProductDetails';
 import Search from './pages/Search';
 import Vision from './pages/Vision';
-import MyProfile from './pages/MyProfile';
+// import MyProfile from './pages/MyProfile';
 import ContactUs from './pages/ContactUs';
 import Return from './pages/Return'
 
 // components
 import Header from './components/Header';
 import Footer from './components/Footer';
-
-
 
 // layout
 const Layout = () => {
@@ -34,21 +32,49 @@ const router = createBrowserRouter([
     element: <Layout />,
     children: [
       { path: '/', element: <Home /> },
+      { path: '/vision', element: <Vision /> },
       { path: '/products/:id', element: <Products /> },
+      { path: '/contactUs', element: <ContactUs /> },
       { path: '/product/:id', element: <ProductDetails /> },
       { path: '/search', element: <Search /> },
-      { path: '/vision', element: <Vision /> },
-      { path: '/myprofile', element: <MyProfile /> },
-      { path: '/contactUs', element: <ContactUs /> },
       { path: '/return', element: <Return /> },
+      // { path: '/myprofile', element: <MyProfile /> },
     ],
   },
 ]);
 
+const generateSitemapXML = () => {
+  const routes = router.getRoutes(); // Get all the routes defined in the router
+
+  const sitemap = `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${routes.map(route => {
+        const url = `https://www.indsoftware.in${route.path}`; // Update with your actual domain
+        return `
+          <url>
+            <loc>${url}</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+          </url>
+        `;
+      }).join('')}
+    </urlset>
+  `;
+
+  return sitemap;
+};
+
 const App = () => {
+  const sitemap = generateSitemapXML();
+
   return (
     <div>
       <RouterProvider router={router} />
+      <textarea
+        style={{ width: '100%', height: '500px' }}
+        value={sitemap}
+        readOnly
+      />
     </div>
   );
 };
