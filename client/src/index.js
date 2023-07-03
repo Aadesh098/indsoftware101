6834c1d4afbe7c3c0520834411debb27ee5ebd84
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2023-07-03 10:36:34 +0530

    r

--- client/src/index.js
+++ client/src/index.js
@@ -2,17 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { HelmetProvider } from 'react-helmet-async'; // Import HelmetProvider
 // cart provider
 import CartProvider from './context/CartContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HelmetProvider> {/* Wrap your components with HelmetProvider */}
       <CartProvider>
         <App />
       </CartProvider>
-    </HelmetProvider>
   </React.StrictMode>
 );
