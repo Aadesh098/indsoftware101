61141200495738768bf748f77d0361e334bde3e5
Aadesh098 <48157409+Aadesh098@users.noreply.github.com>
2023-07-04 12:33:01 +0530

    Linkin

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaYoutube, FaInstagram, FaFacebook} from 'react-icons/fa';
+import { FaYoutube, FaInstagram, FaFacebook, FaLinkedin} from 'react-icons/fa';
 
 const Footer = () => {
   return (
@@ -37,14 +37,17 @@ const Footer = () => {
         </div>
         {/* socials */}
         <div className='flex gap-x-6 max-w-max mx-auto text-lg mb-16'>
-          <a href='https://www.instagram.com/_indsoftware_/' className='hover:text-white transition-all'>
-          <FaInstagram />
+        <a href='https://www.facebook.com/IndSoftwarez' className='hover:text-white transition-all'>
+            <FaFacebook />
           </a>
           <a href='https://www.youtube.com/channel/UCApQohhdp15MngK2-c2wxLA' className='hover:text-white transition-all'>
           <FaYoutube />
           </a>
-          <a href='https://www.facebook.com/IndSoftwarez' className='hover:text-white transition-all'>
-            <FaFacebook />
+          <a href='https://www.instagram.com/_indsoftware_/' className='hover:text-white transition-all'>
+          <FaInstagram />
+          </a>
+          <a href='https://www.linkedin.com/company/indsoftwarez/' className='hover:text-white transition-all'>
+          <FaLinkedin />
           </a>
         </div>
       </div>
